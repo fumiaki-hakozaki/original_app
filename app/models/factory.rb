5d6ec63e37a3fc3f8ed5e846fa96bb553c7a206d
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-10 15:30:33 +0900

    アソシエーション設定

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,3 +1,8 @@
 class Factory < ApplicationRecord
+  has_many :favorites, dependent: :destroy
+  has_many :reviews
+  has_many :services
+  has_many :factory_features
+  has_many :features, through: :factory_features, source: :feature
   mount_uploader :image,ImageUploader
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,4 @@
 class Feature < ApplicationRecord
+  has_many :factories, through: :factory_features, source: :factory
+  has_many :factory_features
 end
