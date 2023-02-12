b585bcd60405c7999362764d8b63da110a93e795
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-12 23:48:20 +0900

    featureタグ付け機能実装

--- app/models/factory.rb
+++ app/models/factory.rb
@@ -1,7 +1,7 @@
 class Factory < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :services
   has_many :factory_features
   has_many :features, through: :factory_features, source: :feature
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :factory
+  validates :content, presence: true
+  mount_uploader :image, ImageUploader
 end
