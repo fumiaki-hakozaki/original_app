5dbd2323f68ca0a25db8c9b5466f1e76d318080d
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-16 17:34:50 +0900

    サイト開いてすぐtopページに遷移

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get :favorites, on: :collection
   end
   root to: "factories#top"
-  get 'factories/top'
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
