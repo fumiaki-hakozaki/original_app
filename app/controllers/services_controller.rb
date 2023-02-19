bf54210a6bd6c7302aedb7a264a01c33dece27f8
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-19 16:40:19 +0900

    serviseネストした後のpath修正

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,11 @@
 class ServicesController < ApplicationController
   before_action :set_service, only: %i[ show edit update destroy ]
+  before_action :set_factory, only: %i[ edit update ]
 
   # GET /services or /services.json
   def index
     @services = Service.all
+    set_factory
   end
 
   # GET /services/1 or /services/1.json
@@ -12,12 +14,13 @@ class ServicesController < ApplicationController
 
   # GET /services/new
   def new
+    @factory = Factory.find(params[:factory_id])
     @service = Service.new
   end
 
   # GET /services/1/edit
   def edit
-  @factory = Factory.find(params[:factory_id])
+    @factory = @service.factory
   end
 
   # POST /services or /services.json
@@ -26,7 +29,7 @@ class ServicesController < ApplicationController
     @service = factory.services.build(service_params)
     respond_to do |format|
       if @service.save
-        format.html { redirect_to factory_service_path(factory, @service), notice: "Service was successfully created." }
+        format.html { redirect_to factory_service_path(factory, @service),notice: "Service was successfully created." }
         format.json { render :show, status: :created, location: @service }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +42,7 @@ class ServicesController < ApplicationController
   def update
     respond_to do |format|
       if @service.update(service_params)
-        format.html { redirect_to factory_service_path(factory, @service), notice: "Service was successfully updated." }
+        format.html { redirect_to factory_service_path(@factory, @service), notice: "Service was successfully updated." }
         format.json { render :show, status: :ok, location: @service }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -64,8 +67,16 @@ class ServicesController < ApplicationController
       @service = Service.find(params[:id])
     end
 
+    def set_factory
+      if @service.present?
+        @factory = @service.factory
+      else
+        @factory = Factory.find(params[:factory_id])
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def service_params
-      params.require(:service).permit(:service_name, :price, :content)
+      params.require(:service).permit(:service_name, :price, :content, :id).merge(factory_id: params[:factory_id])
     end
 end
