176a9eb9e7be6d5db828b42e11e7b35290e052a0
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-21 18:28:32 +0900

    Rspec,Userテーブル

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
+
   factory :user do
-    
+    name { "nagasaki" }
+    email { "example@example.com" }
+    password { "password" }
+    profile { "私のプロフィール" }
+    admin { false }
+  end
+
+  factory :secound_user, class:User do
+    name { "fukuoka" }
+    email { "fukuoka@example.com" }
+    password { "password" }
+    profile { "僕のプロフィール" }
+    admin { false }
+  end
+  factory :third_user, class:User do
+    name { "tokyo" }
+    email { "tokyo@example.com" }
+    password { "password" }
+    profile { "東京プロフィール" }
+    admin { true }
   end
 end
