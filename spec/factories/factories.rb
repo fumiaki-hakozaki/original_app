4e465102163a45be5fa176085144075443354d96
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-02-23 03:32:25 +0900

    review,Rspecテスト

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :factory do
-    
+    factory_name { 'ABC工場' }
+    summary { '楽しい' }
+    address { '北海道' }
+    latitude { 35.123456 }
+    longitude { 135.123456 }
+  end
+
+  factory :factory_2, class:User do
+    factory_name { 'あいうえオート' }
+    summary { 'いい感じ' }
+    address { '大阪' }
+    latitude { 35.123456 }
+    longitude { 135.123456 }
+  end
+
+  factory :factory_3, class:User do
+    factory_name { 'サービスサービス' }
+    summary { '大型もできます' }
+    address { '新潟県' }
+    latitude { 35.123456 }
+    longitude { 135.123456 }
   end
 end
