186b96dd39e3fcc5ab7f6d7cd4d0d9f08220c22e
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2023-04-23 23:40:27 +0900

    init unicorn2回目

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '3.113.236.28', user: 'app', roles: %w{app db web}
+server '13.114.79.191', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/hakozakifumiaki/.ssh/id_rsa'
 
 
